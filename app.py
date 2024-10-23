79fb9526d269b47bd4ccbb5c321a802a3b9d524a
Bruno Bevilacqua Rizzi <45837583+BrunoRizzi@users.noreply.github.com>
2024-10-22 22:03:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -186,5 +186,6 @@ def identify_card():
     return jsonify(response), 200
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=5000, debug=True)
 
